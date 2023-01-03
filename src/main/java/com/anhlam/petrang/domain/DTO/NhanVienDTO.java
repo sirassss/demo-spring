0f314cc15e5d5e6504c0211f6ab879f58e982429
lamta <lamta@softdreams.vn>
2023-01-03 23:43:15 +0700

    update

--- src/main/java/com/anhlam/petrang/domain/DTO/NhanVienDTO.java
+++ src/main/java/com/anhlam/petrang/domain/DTO/NhanVienDTO.java
@@ -1,7 +1,10 @@
 package com.anhlam.petrang.domain.DTO;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 
+@Data
 public class NhanVienDTO {
 
     private Long id;
@@ -11,60 +14,4 @@ public class NhanVienDTO {
     private String soDT;
     private String email;
     private String tenPhong;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTenNV() {
-        return tenNV;
-    }
-
-    public void setTenNV(String tenNV) {
-        this.tenNV = tenNV;
-    }
-
-    public Boolean getGioiTinh() {
-        return gioiTinh;
-    }
-
-    public void setGioiTinh(Boolean gioiTinh) {
-        this.gioiTinh = gioiTinh;
-    }
-
-    public String getDiaChi() {
-        return diaChi;
-    }
-
-    public void setDiaChi(String diaChi) {
-        this.diaChi = diaChi;
-    }
-
-    public String getSoDT() {
-        return soDT;
-    }
-
-    public void setSoDT(String soDT) {
-        this.soDT = soDT;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTenPhong() {
-        return tenPhong;
-    }
-
-    public void setTenPhong(String tenPhong) {
-        this.tenPhong = tenPhong;
-    }
 }
