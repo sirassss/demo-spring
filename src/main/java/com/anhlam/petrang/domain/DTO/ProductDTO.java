17d9d110fdda0d31e10c644211ef731711d64aad
lamta <lamta@softdreams.vn>
2023-01-04 09:36:13 +0700

    up

--- src/main/java/com/anhlam/petrang/domain/DTO/ProductDTO.java
+++ src/main/java/com/anhlam/petrang/domain/DTO/ProductDTO.java
@@ -1,9 +1,10 @@
 package com.anhlam.petrang.domain.DTO;
 
 import com.anhlam.petrang.domain.HangSX;
+import lombok.Data;
 
 import java.math.BigDecimal;
-
+@Data
 public class ProductDTO {
 
     private Long id;
@@ -23,76 +24,4 @@ public class ProductDTO {
     private String moTa;
 
     private HangSX hangSX;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getHangSXID() {
-        return hangSXID;
-    }
-
-    public void setHangSXID(Long hangSXID) {
-        this.hangSXID = hangSXID;
-    }
-
-    public String getTenSP() {
-        return tenSP;
-    }
-
-    public void setTenSP(String tenSP) {
-        this.tenSP = tenSP;
-    }
-
-    public Integer getSoLuong() {
-        return soLuong;
-    }
-
-    public void setSoLuong(Integer soLuong) {
-        this.soLuong = soLuong;
-    }
-
-    public String getMauSac() {
-        return mauSac;
-    }
-
-    public void setMauSac(String mauSac) {
-        this.mauSac = mauSac;
-    }
-
-    public BigDecimal getGiaBan() {
-        return giaBan;
-    }
-
-    public void setGiaBan(BigDecimal giaBan) {
-        this.giaBan = giaBan;
-    }
-
-    public String getDonViTinh() {
-        return donViTinh;
-    }
-
-    public void setDonViTinh(String donViTinh) {
-        this.donViTinh = donViTinh;
-    }
-
-    public String getMoTa() {
-        return moTa;
-    }
-
-    public void setMoTa(String moTa) {
-        this.moTa = moTa;
-    }
-
-    public HangSX getHangSX() {
-        return hangSX;
-    }
-
-    public void setHangSX(HangSX hangSX) {
-        this.hangSX = hangSX;
-    }
 }
